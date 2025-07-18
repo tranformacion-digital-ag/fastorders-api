16c4f2cfc07075a348f7a83f10a4ef47eac25fd6
Luis Ycaza <lycaza@agripac.com.ec>
2025-07-17 21:49:41 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+
+    """
+    Metodo para retornar el nombre
+
+    """
+    return {"message": f"Hello {name}"}
